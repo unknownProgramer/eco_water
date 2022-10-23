488550ceb68791190b30793ae54a711373c58397
sksms0431@naver.com <sksms0431@naver.com>
2022-10-23 23:24:45 +0900

    2022.10.23_불필요한 모듈 삭제

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'assets')));
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')))
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
+// app.use('views/textures/cube/', express.static(path.join(__dirname, 'views/textures/cube/')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
