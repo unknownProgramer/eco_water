87e493fa59945f3e2e87fd8996570c8c0ee0fc83
sksms0431@naver.com <sksms0431@naver.com>
2022-11-08 21:01:46 +0900

    2022.11.08_modelupdate

--- routes/pred.js
+++ routes/pred.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('test_pred');
+    res.render('pred');
 });
 
 module.exports = router;
